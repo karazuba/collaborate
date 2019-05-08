62d0c0b516a3edc1b98ba11d66fda0330f61485b
karazuba <jaroslav.gavrilenko@gmail.com>
2019-05-08 19:51:22 +0300

    Added current user profile permission

--- account/api/permissions.py
+++ account/api/permissions.py
@@ -6,3 +6,8 @@ class IsCurrentUserProfileOrReadOnly(BasePermission):
         if request.method in SAFE_METHODS:
             return True
         return request.user.profile.id == request.resolver_match.kwargs.get('pk')
+
+
+class IsCurrentUserProfile(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.profile.id == request.resolver_match.kwargs.get('pk')
