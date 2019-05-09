506dff549e853f84cb60720ecaf95ed7dfe763be
karazuba <jaroslav.gavrilenko@gmail.com>
2019-05-09 22:57:36 +0300

    Added bookmark article view test

--- bookmarks/api/urls.py
+++ bookmarks/api/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from bookmarks.api.views import ArticleBookmarkList, ChangeArticleBookmark
 
 urlpatterns = [
-    path('profiles/<int:pk>/bookmarks/articles/', ArticleBookmarkList.as_view()),
+    path('profiles/<int:pk>/bookmarks/articles/',
+         ArticleBookmarkList.as_view(), name='article-bookmark-list'),
     path('profiles/<int:pk>/bookmarks/articles/<int:article_pk>/',
-         ChangeArticleBookmark.as_view()),
+         ChangeArticleBookmark.as_view(), name='article-bookmark-change'),
 ]
